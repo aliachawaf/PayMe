5203d3fe4245a2bc5abdfffbf1acaa65809265e1
aliachawaf <aliachawaf@gmail.com>
2019-03-24 11:05:20 +0100

    Initial Commit

---
+++ PayMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayMe
+//
+//  Created by Alia Chawaf on 24/03/2019.
+//  Copyright © 2019 Chawaf Alia & Gestin Rémi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PayMeTests/PayMeTests.swift
@@ -0,0 +1,34 @@
+//
+//  PayMeTests.swift
+//  PayMeTests
+//
+//  Created by Alia Chawaf on 24/03/2019.
+//  Copyright © 2019 Chawaf Alia & Gestin Rémi. All rights reserved.
+//
+
+import XCTest
+@testable import PayMe
+
+class PayMeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
